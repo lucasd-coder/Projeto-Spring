826c8aa574819eb42157a857878d91037815c0be
Lucas Silva <lucasdasilva524@hotmail.com>
2020-09-19 15:42:29 -0300

    Configurando front-web para produção

--- front-web/src/pages/Charts/index.tsx
+++ front-web/src/pages/Charts/index.tsx
@@ -23,7 +23,7 @@ const initialPieData = {
     series: []
 }
 
-const BASE_URL = 'http://localhost:8080';
+const BASE_URL = 'https://spring1-lucas.herokuapp.com';
 
 const Charts = () => {
     const [barChartData, setBarChartData] = useState<BarChartData[]>([]);
--- front-web/src/pages/Records/index.tsx
+++ front-web/src/pages/Records/index.tsx
@@ -10,7 +10,7 @@ import Filters from '../../components/Filters';
 
 import './styles.css';
 
-const BASE_URL = 'http://localhost:8080';
+const BASE_URL = 'https://spring1-lucas.herokuapp.com';
 
 const Records = () => {
 
